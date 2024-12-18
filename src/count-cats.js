a3714a93497ddfcdb6edeaed029e1ee5a151d42f
DenisNedelko <dnsndlk@gmail.com>
2024-12-18 17:05:04 +0300

    feat: add solutions for count-cats.js and dream-team.js 2024-12-18 17:05:04

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  return matrix.reduce((acc, item) => {
+    return acc + item.filter(elem => elem === '^^').length
+  },0)
 }
 
 module.exports = {
--- src/dream-team.js
+++ src/dream-team.js
@@ -2,22 +2,31 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+	if (!Array.isArray(members)) return false;
+	return members
+		.reduce((acc, item) => {
+			if (typeof item === 'string') {
+				item = item.trim();
+				acc.push(item[0].toUpperCase());
+			}
+			return acc;
+		}, [])
+		.sort((a, b) => a.localeCompare(b))
+		.join('');
 }
 
 module.exports = {
-  createDreamTeam
+	createDreamTeam,
 };
