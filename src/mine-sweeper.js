c54d55ac6fa9569bfae106287acedc8f7e149d28
DenisNedelko <dnsndlk@gmail.com>
2024-12-22 22:21:59 +0300

    feat: add solution for mini-sweeper.js 2024-12-22 22:21:59

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+	const result = new Array(matrix.length);
+
+	for (let i = 0; i < matrix.length; i++) {
+		result[i] = [];
+
+		for (let j = 0; j < matrix[i].length; j++) {
+			let count = 0;
+			let leftItem = j > 0 ? matrix[i][j - 1] : false;
+			let rightItem = j < matrix[i].length - 1 ? matrix[i][j + 1] : false;
+			let topItem = i > 0 ? matrix[i - 1][j] : false;
+			let bottomItem = i < matrix.length - 1 ? matrix[i + 1][j] : false;
+			let leftTopItem = i > 0 && j > 0 ? matrix[i - 1][j - 1] : false;
+			let leftBottomItem =
+				i < matrix.length - 1 && j > 0 ? matrix[i + 1][j - 1] : false;
+			let rightTopItem =
+				i > 0 && j < matrix[i].length - 1 ? matrix[i - 1][j + 1] : false;
+			let rightBottomItem =
+				i < matrix.length - 1 && j < matrix[i].length - 1
+					? matrix[i + 1][j + 1]
+					: false;
+      if(rightItem) count++
+			if (leftItem) count++;
+			if (topItem) count++;
+			if (bottomItem) count++;
+			if (leftTopItem) count++;
+			if (leftBottomItem) count++;
+      if (rightTopItem) count++;
+			if (rightBottomItem) count++;
+			result[i].push(count);
+			count = 0;
+		}
+	}
+	return result;
 }
 
 module.exports = {
