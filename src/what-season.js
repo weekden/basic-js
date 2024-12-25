c160e0d2c89248a93a4d680526e9a706f86dd8dc
DenisNedelko <dnsndlk@gmail.com>
2024-12-25 20:38:50 +0300

    feat: add hanoi-tower.js and what-season.js 2024-12-25 20:38:50

--- src/what-season.js
+++ src/what-season.js
@@ -11,33 +11,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-function getSeason(/* date */) {
-	// if (!date) return 'Unable to determine the time of year!';
-	// if (!(date instanceof Date)) throw new Error('Invalid date!');
-	// const monthNumber = date.getMonth() + 1;
-	// switch (monthNumber) {
-	// 	case 12:
-	// 	case 1:
-	// 	case 2:
-	// 		return 'winter';
-	// 	case 3:
-	// 	case 4:
-	// 	case 5:
-	// 		return 'spring';
-	// 	case 6:
-	// 	case 7:
-	// 	case 8:
-	// 		return 'summer';
-	// 	case 9:
-	// 	case 10:
-	// 	case 11:
-	// 		return 'autumn';
-	// 	default:
-	// 		return;
-	// }
+function getSeason(date) {
+	if (!date) return 'Unable to determine the time of year!';
+	if (!(date instanceof Date)) throw new Error('Invalid date!');
+	const monthNumber = date.getMonth() + 1;
+  
+	switch (monthNumber) {
+		case 12:
+		case 1:
+		case 2:
+			return 'winter';
+		case 3:
+		case 4:
+		case 5:
+			return 'spring';
+		case 6:
+		case 7:
+		case 8:
+			return 'summer';
+		case 9:
+		case 10:
+		case 11:
+			return 'autumn';
+	}
 
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
 }
 
 module.exports = {
